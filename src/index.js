93976ff8b5c91562337e8958c7efb6adebee9a20
Justin Brown <justinbrown.work@gmail.com>
2019-05-20 15:50:40 -0400

    Add Heading component.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export * from "./Badge";
 export * from "./Box";
 export * from "./Button";
 export * from "./Flex";
+export * from "./Heading";
 export * from "./Input";
 export * from "./Text";
 export { default as theme } from "./theme";
