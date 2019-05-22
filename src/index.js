b98ddbaed5f58513e389bba42ad0eae3ec4d8b74
Justin Brown <justinbrown.work@gmail.com>
2019-05-21 22:03:12 -0400

    Add OutlineButton component.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ export * from "./Button";
 export * from "./Flex";
 export * from "./Heading";
 export * from "./Input";
+export * from "./OutlineButton";
 export * from "./Text";
 export * from "./ToggleBadge";
 export { default as theme } from "./theme";
